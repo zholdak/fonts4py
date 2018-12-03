fd6c1fa7dd1cd49898147b802ceb972a7ec37547
Oleksiy Zholdak <oleksiy.zholdak@lifecell.com.ua>
2018-12-03 09:29:25 +0200

    fonts and xbm images now repacks into bytearrays instead of int arrays to substantially decrease memory footprint

--- convert.sh
+++ convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://github.com/olikraus/u8glib/wiki/fontsize
+
 python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont11.py pyfonts/pyfull/font_profont11_6x7_full.py --charset @full_encoding.txt
 
 ./ttf/bdf2u8g -b 32 -e 254 -f 0 u8g_tools/bdf/helvB08.bdf u8g_font_helvB08 pyfonts/u8g/u8g_font_helvB08.py
@@ -24,31 +26,15 @@ python3 u8gfont_repack.py pyfonts/u8g/u8g_font_lato_bold_58.py pyfonts/pyfull/fo
 ######
 
 python3 u8gfont_repack.py pyfonts/pyfull/font_profont11_6x7_full.py pyfonts/font_profont11_6x7_numbers.py --charset 1234567890.-
+python3 u8gfont_repack.py pyfonts/pyfull/font_helvB08_full.py pyfonts/font_helvB08_full.py --charset @full_encoding.txt
 
 python3 u8gfont_repack.py pyfonts/pyfull/font_lato_bold_21_full.py pyfonts/font_lato_bold_21_degC.py --charset °C
 python3 u8gfont_repack.py pyfonts/pyfull/font_lato_bold_33_full.py pyfonts/font_lato_bold_33_numbers.py --charset 1234567890.
 python3 u8gfont_repack.py pyfonts/pyfull/font_lato_bold_40_full.py pyfonts/font_lato_bold_40_numbers.py --charset 1234567890.
 python3 u8gfont_repack.py pyfonts/pyfull/font_lato_bold_58_full.py pyfonts/font_lato_bold_58_numbers.py --charset 1234567890-
 
-######
 
-# https://github.com/olikraus/u8glib/wiki/fontsize
+###### XBM IMAGES
 
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_baby.py pyfonts/pyfull/font_5x_baby_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_micro.py pyfonts/pyfull/font_5x_micro_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_4x6.py pyfonts/fpyfull/ont_4x6_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_5x8.py pyfonts/pyfull/font_5x8_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_fixed_v0.py pyfonts/pyfull/font_fixed_v0_7x7_full.py --charset @full_encoding.txt
-
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont10.py pyfonts/pyfull/font_profont10_5x6_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont11.py pyfonts/pyfull/font_profont11_6x7_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont12.py pyfonts/pyfull/font_profont12_6x8_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont15.py pyfonts/pyfull/font_profont15_7x9_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont17.py pyfonts/pyfull/font_profont17_9x11_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont22.py pyfonts/pyfull/font_profont22_12x14_full.py --charset @full_encoding.txt
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_profont29.py pyfonts/pyfull/font_profont29_16x19_full.py --charset @full_encoding.txt
-
-python3 u8gfont_repack.py pyfonts/u8g/u8g_font_lato_bold_33.py pyfonts/pyfull/font_lato_bold_33_full.py --charset @full_encoding.txt
-
-######
-
+python3 xbm_repack.py gfx/mmhg.xbm gfx/mmhg_xbm.py
+python3 xbm_repack.py gfx/pc_rh.xbm gfx/pc_rh_xbm.py
--- u8gfont_repack.py
+++ u8gfont_repack.py
@@ -8,49 +8,7 @@ import sys
 
 from u8g.font import U8GFont
 from u8g.glyph import U8GGlyph
-
-
-class ByteWriter(object):
-    bytes_per_line = 16
-
-    def __init__(self, stream, varname):
-        self.stream = stream
-        self.stream.write('{} = [\n'.format(varname))
-        self.bytecount = 0  # For line breaks
-        self.bytes_in_line = 0  # For line breaks
-
-    def _eol(self):
-        self.stream.write("\n")
-
-    def _eot(self):
-        self.stream.write("\n")
-
-    def _bol(self):
-        self.stream.write("    ")
-
-    # Output a single byte
-    def out_byte(self, byte):
-        if self.bytecount:
-            self.stream.write(',')
-        if self.bytecount and not self.bytes_in_line:
-            self._eol()
-        if not self.bytes_in_line:
-            self._bol()
-        self.stream.write('{:d}'.format(byte))
-        self.bytes_in_line += 1
-        self.bytes_in_line %= self.bytes_per_line
-        self.bytecount += 1
-
-    # Output from a sequence
-    def out_data(self, bytelist):
-        for byt in bytelist:
-            self.out_byte(byt)
-
-    # ensure a correct final line
-    def eot(self):  # User force EOL if one hasn't occurred
-        if self.bytes_in_line:
-            self._eot()
-        self.stream.write(']\n')
+from utils.bytewriter import ByteAsBytearrayWriter
 
 
 def char_info(char_enc: int):
@@ -134,7 +92,8 @@ def repack_and_write(stream, font_data, charset):
 
     stream.write("# {}\n".format(repr(font2)))
     stream.write('all_chars = """{}"""\n'.format(font2.get_all_chars()))
-    bw_font = ByteWriter(stream, 'data')
+    # bw_font = ByteAsIntArrayWriter(stream, 'data')
+    bw_font = ByteAsBytearrayWriter(stream, 'data')
     bw_font.out_data(repacked_font_header)
     bw_font.out_data(repacked_characters)
     bw_font.eot()
